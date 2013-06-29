7cd1b4767810b2816ef89575b1c649f2ba1ea3bf
Derek <derekr@me.com>
2013-06-29 16:11:25 -0700

    Initial commit
    Rendering basic mapbox map

---
+++ bin/app.js
@@ -0,0 +1,35 @@
+var path  = require('path');
+var fours = require('fours')(process.env.FOURS_ID, process.env.FOURS_SECRET);
+
+var express = require('express');
+var app     = express();
+var hbs     = require('express-hbs');
+
+app.engine('html', hbs.express3({
+  partialsDir: path.resolve(__dirname, '../lib/views/partials'),
+  contentHelperName: 'content',
+  extname: '.html'
+}));
+
+app.set('view engine', 'html');
+app.set('views', path.resolve(__dirname, '../lib/views'));
+
+app.use('/public', express.static(path.join(__dirname, '../lib/public')));
+
+app.get('/', function (req, res){
+  res.render('index', {
+      title: 'Where Da Movies At',
+      layout: '_layout'
+    });
+});
+
+app.get('/fours/*', function (req, res) {
+    fours({
+        method: req.method,
+        uri: req.url.replace(/\/fours/, '')
+    }).pipe(res);
+});
+
+var port = process.env.WHEREDAMOVIESAT_PORT || 3000;
+console.log('Starting wheredamoviesat on port ' + port);
+app.listen(port);
---
+++ makefile
@@ -0,0 +1,4 @@
+run:
+	node ./bin/app.js
+
+.PHONY: run
